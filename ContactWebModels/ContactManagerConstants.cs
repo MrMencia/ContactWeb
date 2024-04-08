bd668bd8e0d70db68b853581441ad70e8df9257b
MrMencia <Avimencia@gmail.com>
2024-04-07 19:51:59 -0500

    Created the models project

---
+++ ContactWebModels/ContactManagerConstants.cs
@@ -0,0 +1,29 @@
+﻿namespace ContactWebModels
+{
+    public class ContactManagerConstants
+    {
+
+        //This is a constant Class
+        // A Connstant Class is used to define constant values that are accessible throughout
+        //the application
+
+        //Constants are static, meaning the belong to the class rather than the instance of the class
+
+        //They are typically used to store values that do not change during the execution
+        //of the application
+
+        public const int MAX_STATE_LENGTH = 50;
+        public const int MAX_STATE_ABBR_LENGTH = 4;
+
+        public const int MAX_FIRST_NAME_LENGTH = 50;
+        public const int MAX_LAST_NAME_LENGTH = 250;
+        public const int MAX_EMAIL_LENGTH = 250;
+        public const int MAX_PHONE_LENGTH = 15;
+        public const int MAX_STREET_ADDRESS_LENGTH = 250;
+        public const int MAX_CITY_LENGTH = 50;
+        public const int MAX_ZIP_CODE_LENGTH = 10;
+        public const int MIN_ZIP_CODE_LENGTH = 5;
+
+
+    }
+}
---
+++ ContactWebModels/State.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactWebModels
+{
+    internal class State
+    {
+        //State Model Class
+
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "State")]
+        [Required(ErrorMessage = "Name of State is Required")]
+        [StringLength(ContactManagerConstants.MAX_STATE_LENGTH)]
+
+
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "State Abbreviation is required")]
+        [StringLength(ContactManagerConstants.MAX_STATE_ABBR_LENGTH)]
+        public string Abbreviation { get; set; }
+
+    }
+}
